b3c5353f4fc137dce52a1e68ffb4c88c7af012c4
Gregory Tereshko <gtrsh@users.noreply.github.com>
2022-06-09 17:11:23 +0300

    register endpoint in app

--- vk-api-stub/src/app.js
+++ vk-api-stub/src/app.js
@@ -3,6 +3,7 @@ import * as fst from 'fastify'
 const createApp = (opts) => {
   const app = fst.fastify(opts)
 
+  app.register(import('./resources/notification')) // eslint-disable-line
   return app
 }
 
