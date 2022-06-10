480d48f13611c70e113e002d9a9008960efd09d4
Gregory Tereshko <gtrsh@users.noreply.github.com>
2022-06-10 23:25:58 +0300

    add custom validation error

--- vk-api-stub/src/resources/notification/notification.router.js
+++ vk-api-stub/src/resources/notification/notification.router.js
@@ -1,7 +1,16 @@
 import { postNotificationSchema as schema } from './notification.validator'
 
 const router = (fastify, opts, done) => {
-  fastify.post('/api/notification', { schema }, async (req) => {
+  fastify.post('/api/notification', { schema, attachValidation: true }, async (req, rep) => {
+    if (req.validationError) {
+      rep
+        .code(400)
+        .send({
+          code: 3,
+          description: 'Invalid data'
+        })
+    }
+
     return {
       data: []
     }
