322c430d5fdb4b97e76ed87e735016fcdcf68e39
Gregory Tereshko <gtrsh@users.noreply.github.com>
2022-06-11 12:31:13 +0300

    update validator

--- vk-api-stub/src/resources/notification/notification.validator.js
+++ vk-api-stub/src/resources/notification/notification.validator.js
@@ -2,7 +2,10 @@ import S from 'fluent-json-schema'
 
 const postNotificationSchema = {
   body: S.object()
-    .prop('ids', S.array().items(S.string()).maxItems(100).required())
+    .prop('ids', S.array().items(S.string())
+      .minItems(1)
+      .maxItems(100)
+      .required())
     .prop('text', S.string().required())
 }
 
