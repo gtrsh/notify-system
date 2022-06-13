566d0b7d21183eff2d7e7b46a44c6361c0c18997
Gregory Tereshko <gtrsh@users.noreply.github.com>
2022-06-13 14:15:21 +0300

    add fn for getting object from array

--- notify-service/src/utils/index.js
+++ notify-service/src/utils/index.js
@@ -1,6 +1,8 @@
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
 
+const unwrap = (arr) => arr[0]
+
 function urlDirname (url) {
   return dirname(fileURLToPath(url))
 }
@@ -12,6 +14,7 @@ function urlJoin (url, ...str) {
 export default urlDirname
 
 export {
+  unwrap,
   fileURLToPath as filename,
   urlJoin as join,
   urlDirname as dirname
